7437ca4d256726a328dfe8116aca306f28c41fa6
kevinloi142 <48134251+kevinloi142@users.noreply.github.com>
2021-06-06 23:13:32 -0700

    Update kloi002_lab11_part0(basic).c

--- kloi002_lab11_part0(basic).c
+++ kloi002_lab11_part0(basic).c
@@ -7,7 +7,7 @@
  *      I acknowledge all content contained herein, excluding template or example
  *      code, is my own original work.
  *
- *      Demo Link: 
+ *      Demo Link: https://drive.google.com/file/d/1vfaKUjg6N-pXka62Kf-GSFtbRuz4Xylm/view?usp=sharing
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
